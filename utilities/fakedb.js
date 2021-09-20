c3af7ad47c04e9370ba5474a2cb1d4a6bbddc0ac
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2021-09-20 16:11:12 -0500

    Update and rename databaseManager.js to fakedb.js

---
+++ utilities/fakedb.js
@@ -0,0 +1,42 @@
+// use localstorage as your db for now
+const addToDb = id => {
+    const exists = getDb();
+    let shopping_cart = {};
+    if (!exists) {
+        shopping_cart[id] = 1;
+    }
+    else {
+        shopping_cart = JSON.parse(exists);
+        if (shopping_cart[id]) {
+            const newCount = shopping_cart[id] + 1;
+            shopping_cart[id] = newCount;
+        }
+        else {
+            shopping_cart[id] = 1;
+        }
+    }
+    updateDb(shopping_cart);
+}
+
+const getDb = () => localStorage.getItem('shopping_cart');
+const updateDb = cart => {
+    localStorage.setItem('shopping_cart', JSON.stringify(cart));
+}
+
+const removeFromDb = id => {
+    const exists = getDb();
+    if (!exists) {
+
+    }
+    else {
+        const shopping_cart = JSON.parse(exists);
+        delete shopping_cart[id];
+        updateDb(shopping_cart);
+    }
+}
+
+const clearTheCart = () => {
+    localStorage.removeItem('shopping_cart');
+}
+
+export { addToDb, removeFromDb as deleteFromDb, clearTheCart }
