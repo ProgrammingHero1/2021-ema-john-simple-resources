4c5c5ce8c3576e3896427fed4e9c5b9ab9ea25ba
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2021-09-20 16:32:57 -0500

    Update fakedb.js

--- utilities/fakedb.js
+++ utilities/fakedb.js
@@ -1,42 +1,47 @@
-// use localstorage as your db for now
+// use local storage as your db for now
 const addToDb = id => {
-    const exists = getDb();
-    let shopping_cart = {};
-    if (!exists) {
-        shopping_cart[id] = 1;
+  const exists = getDb();
+  let shopping_cart = {};
+  if (!exists) {
+    shopping_cart[id] = 1;
+  }
+  else {
+    shopping_cart = JSON.parse(exists);
+    if (shopping_cart[id]) {
+      const newCount = shopping_cart[id] + 1;
+      shopping_cart[id] = newCount;
     }
     else {
-        shopping_cart = JSON.parse(exists);
-        if (shopping_cart[id]) {
-            const newCount = shopping_cart[id] + 1;
-            shopping_cart[id] = newCount;
-        }
-        else {
-            shopping_cart[id] = 1;
-        }
+      shopping_cart[id] = 1;
     }
-    updateDb(shopping_cart);
+  }
+  updateDb(shopping_cart);
 }
 
 const getDb = () => localStorage.getItem('shopping_cart');
 const updateDb = cart => {
-    localStorage.setItem('shopping_cart', JSON.stringify(cart));
+  localStorage.setItem('shopping_cart', JSON.stringify(cart));
 }
 
 const removeFromDb = id => {
-    const exists = getDb();
-    if (!exists) {
+  const exists = getDb();
+  if (!exists) {
 
-    }
-    else {
-        const shopping_cart = JSON.parse(exists);
-        delete shopping_cart[id];
-        updateDb(shopping_cart);
-    }
+  }
+  else {
+    const shopping_cart = JSON.parse(exists);
+    delete shopping_cart[id];
+    updateDb(shopping_cart);
+  }
+}
+
+const getStoredCart = () => {
+  const exists = getDb();
+  return exists ? JSON.parse(exists) : {};
 }
 
 const clearTheCart = () => {
-    localStorage.removeItem('shopping_cart');
+  localStorage.removeItem('shopping_cart');
 }
 
-export { addToDb, removeFromDb as deleteFromDb, clearTheCart }
+export { addToDb, removeFromDb as deleteFromDb, clearTheCart, getStoredCart }
